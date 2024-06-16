093ffa880b4861e8657e1ec021fa73d50b7da219
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-06-16 06:54:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
+OWNER_ID = getenv("OWNER_ID", "6961211249")
 SUPPORT_GRP = "THE_FUCKER_BOTS_2926"
 UPDATE_CHNL = "THE_FUCKING_BOT_2926"
 OWNER_USERNAME = "its_innocent_boy_2926"
